47b52343221b7ce8fc1083fdca3713f2496d9abc
Hydro <Elpacolamastica@gmail.com>
2025-04-23 14:07:54 +0200

    Hola

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hola")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hola():
+    lista = [
+        "hola",
+        "adios",
+        "buenos dias",
+        "buenas noches"
+    ]
+
+    for i in lista:
+        return "<br>".join(lista) # esto es para recorrer una lista 
